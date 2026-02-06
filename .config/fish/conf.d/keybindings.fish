3998133c00f29340fe7c01835d727ea558665f94
Branch Vincent <branchevincent@gmail.com>
2026-02-06 13:11:08 -0800

    fish: update keybindings

--- .config/fish/conf.d/keybindings.fish
+++ .config/fish/conf.d/keybindings.fish
@@ -2,21 +2,15 @@
 
 status is-interactive || exit
 
-bind \cc 'commandline ""'
-bind \cl "
-clear; printf '\e[3J'; commandline -f repaint
-if functions -q tide_prompt_at_bottom
-    tide_prompt_at_bottom
-end
-"
-bind \cg 'gh pr view --web &>/dev/null || gh repo view --web &>/dev/null'
-bind \cp workon
-bind \ct 'pushd 2>/dev/null || pushd (mktemp -d) && commandline -f repaint'
-bind \cu '
+bind ctrl-g 'gh pr view --web &>/dev/null || gh repo view --web &>/dev/null'
+bind ctrl-l "clear; printf '\e[3J\e[%s;1H' \$LINES; commandline -f repaint"
+bind ctrl-p workon
+bind ctrl-t 'pushd 2>/dev/null || pushd (mktemp -d) && commandline -f repaint'
+bind ctrl-u '
 if not set -e GIT_DIR GIT_WORK_TREE
     set -gx GIT_DIR "$XDG_DATA_HOME/yadm/repo.git"
     set -gx GIT_WORK_TREE ~
 end
 commandline -f repaint
 '
-fzf_configure_bindings --directory=\cf
+fzf_configure_bindings --directory=ctrl-f
