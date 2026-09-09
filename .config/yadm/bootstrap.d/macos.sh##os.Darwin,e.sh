27cd845619cc09e5945155031d1ebc21ff243e99
Branch Vincent <branchevincent@gmail.com>
2026-09-08 21:33:39 -0700

    macos: replace dockutil with defaults write

--- .config/yadm/bootstrap.d/macos.sh##os.Darwin,e.sh
+++ .config/yadm/bootstrap.d/macos.sh##os.Darwin,e.sh
@@ -26,19 +26,21 @@ defaults write com.apple.dock minimize-to-application -int 1 # Minimize apps int
 defaults write com.apple.dock magnification -int 1           # Enable magnification
 defaults write com.apple.dock largesize -int 80
 if [ "${YADM_CLASS:?}" = "Home" ]; then
-    dockutil --no-restart --remove all
-    dockutil --no-restart --add /System/Applications/Calendar.app
-    dockutil --no-restart --add /System/Applications/Mail.app
-    dockutil --no-restart --add /System/Applications/Messages.app
-    dockutil --no-restart --add /Applications/Slack.app
-    dockutil --no-restart --add /Applications/Spotify.app
-    dockutil --no-restart --add /Applications/Google\ Chrome.app
-    dockutil --no-restart --add /Applications/Visual\ Studio\ Code.app
-    dockutil --no-restart --add /Applications/Zed.app
-    dockutil --no-restart --add /Applications/Ghostty.app
-    dockutil --no-restart --add /System/Applications/System\ Settings.app
-    dockutil --no-restart --add ~/Documents --sort name --display folder --view list
-    dockutil --no-restart --add ~/Downloads --sort dateadded --display folder --view fan
+    dock_app() { printf '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>file://%s/</string><key>_CFURLStringType</key><integer>15</integer></dict></dict></dict>' "${1%/}"; }
+    dock_folder() { printf '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>file://%s/</string><key>_CFURLStringType</key><integer>15</integer></dict><key>sortby</key><integer>%d</integer><key>displayas</key><integer>%d</integer><key>showas</key><integer>%d</integer></dict><key>tile-type</key><string>directory-tile</string></dict>' "${1%/}" "$2" "$3" "$4"; }
+    defaults write com.apple.dock persistent-apps -array \
+        "$(dock_app /System/Applications/Calendar.app)" \
+        "$(dock_app /System/Applications/Mail.app)" \
+        "$(dock_app /System/Applications/Messages.app)" \
+        "$(dock_app /Applications/Slack.app)" \
+        "$(dock_app /Applications/Spotify.app)" \
+        "$(dock_app /Applications/Google\ Chrome.app)" \
+        "$(dock_app /Applications/Zed.app)" \
+        "$(dock_app /Applications/Ghostty.app)" \
+        "$(dock_app /System/Applications/System\ Settings.app)"
+    defaults write com.apple.dock persistent-others -array \
+        "$(dock_folder ~/Documents 1 1 3)" \
+        "$(dock_folder ~/Downloads 2 1 1)"
 fi
 
 ### Finder ###
